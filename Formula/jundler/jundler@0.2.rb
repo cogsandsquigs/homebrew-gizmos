d2074188c91609f322ddefb8ab019f44b937141e
Ian Pratt <ianjdpratt@gmail.com>
2024-07-09 19:03:43 -0500

    Update versions

---
+++ Formula/jundler/jundler@0.2.rb
@@ -0,0 +1,27 @@
+class JundlerAT02 < Formula
+	desc "The JavaScript executable bundler for Node.js projects"
+	homepage "https://github.com/cogsandsquigs/jundler"
+	license "MIT"
+	url "https://github.com/cogsandsquigs/jundler.git", tag: "v0.2.5" # The tag of the release you want to install
+	head "https://github.com/cogsandsquigs/jundler.git", branch: "main" # The unstable branch
+
+	depends_on "rust" => :build
+
+	def install
+		system "cargo", "install", *std_cargo_args
+	end
+
+	test do
+		# `test do` will create, run in and delete a temporary directory.
+		#
+		# This test will fail and we won't accept that! For Homebrew/homebrew-core
+		# this will need to be a test that verifies the functionality of the
+		# software. Run the test with `brew test dotbak`. Options passed
+		# to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+		#
+		# The installed folder is not in the path, so use the entire path to any
+		# executables being tested: `system "#{bin}/program", "do", "something"`.
+		system "#{bin}/jundler", "--version"
+	end
+end
+
